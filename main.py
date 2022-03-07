e060301d7c5492afb81d86f8cd7f89a6ac4d0cb4
kirka00 <kirill.kobzev5555@gmail.com>
2022-03-07 19:31:19 +0300

    new

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('start')
+from flask import Flask
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route("/")
+def index():
+    return 'light tracker'
+
+
+def main():
+    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    main()
