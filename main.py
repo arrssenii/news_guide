38ec67c8bb63440b3c8712c0622a411766b32496
kirka00 <kirill.kobzev5555@gmail.com>
2022-03-24 22:15:49 +0300

    concept and maybe start project

--- main.py
+++ main.py
@@ -1,18 +1,106 @@
-from flask import Flask
+from flask import Flask, request, abort, render_template, redirect, url_for
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField
+from wtforms.validators import DataRequired
+import os
+from data.users import User
+from flask_wtf import FlaskForm
+from data import db_session
 
 
+# setup the app
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config['SECRET_KEY'] = 'o_my_god__secret_key'
+
+# setup the login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    hashed_password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторить пароль', validators=[DataRequired()])
+    username = StringField('Имя пользваотеля', validators=[DataRequired()])
+    submit = SubmitField('Зарегистрироваться')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
 
 
 @app.route("/")
+@app.route("/home")
+@login_required
 def index():
-    return 'light tracker'
+    return render_template("index.html", title='Home')
+ 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.hashed_password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+        )
+        user.set_password(form.hashed_password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
-def main():
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(
+            User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+def main():  # main function
+    db_session.global_init("db/database.db")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
     app.run(debug=True)
 
-
+# start server
 if __name__ == '__main__':
     main()
