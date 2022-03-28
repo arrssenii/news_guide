93ac46c7b071ceaa38900ec68c1a9c062311761a
kirka00 <kirill.kobzev5555@gmail.com>
2022-03-29 00:35:57 +0300

    add comments and other

--- check_pass.py
+++ check_pass.py
@@ -1,17 +1,15 @@
-import re
+import re  # импорт регулярных выражений для шустрой проверки
 
 
-def password_check(password):
-    # проверка на длину
-    length_error = len(password) <= 8
+def password_check(password):  # функция проверки пароля
+    length_error = len(password) <= 8  # проверка на длину
     # проверка на наличие цифр
     digit_error = re.search(r"\d", password) is None
     # проверка на наличие букв верхнего регистра
     uppercase_error = re.search(r"[A-Z]", password) is None
     # проверка на наличие букв нижнего регистра
     lowercase_error = re.search(r"[a-z]", password) is None
-    # проверка на наличие символов
-    symbol_error = re.search(
+    symbol_error = re.search(  # проверка на наличие символов
         r"[ !@#$%&'()*+,-./[\\\]^_`{|}~"+r'"]', password) is None
     res = {  # общий сбор
         'Пароль короче 8 символов': length_error,
@@ -20,4 +18,4 @@ def password_check(password):
         'Пароль не содержит букв нижнего регистра': lowercase_error,
         'Пароль не содержит специальных символов': symbol_error,
     }
-    return res
+    return res  # возвращаем выводы проверок
--- main.py
+++ main.py
@@ -1,62 +1,46 @@
 from flask import Flask, request, abort, render_template, redirect, url_for
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField
-from wtforms.validators import DataRequired
 import os
 from data.users import User
-from flask_wtf import FlaskForm
+from data.users import Posts
 from data import db_session
 from check_pass import password_check
-
-
-# setup the app
+from forms import RegisterForm, LoginForm
+# from api import
+# настройки приложения
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'o_my_god__secret_key'
-
-# setup the login manager
+# sнастройки для логинменеджера
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 
-class RegisterForm(FlaskForm):
-    email = EmailField('Почта:', validators=[DataRequired()])
-    hashed_password = PasswordField('Пароль:', validators=[DataRequired()])
-    password_again = PasswordField(
-        'Повторить пароль:', validators=[DataRequired()])
-    username = StringField('Имя пользваотеля:', validators=[DataRequired()])
-    submit = SubmitField('Зарегистрироваться')
-
-
-class LoginForm(FlaskForm):
-    email = EmailField('Почта:', validators=[DataRequired()])
-    password = PasswordField('Пароль:', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить меня')
-    submit = SubmitField('Войти')
-
-
-@app.route("/")
+''' Нужно добваить вывод рвботы с API + подумать над заполнением главной страницы '''
+@app.route("/")  # главная (домашняя) страница
 @app.route("/home")
 @login_required
 def index():
     return render_template("index.html", title='Home')
 
 
+# обработка регистрации пользователя
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        check_password = password_check(form.hashed_password.data)
+        check_password = password_check(
+            form.hashed_password.data)  # проверка пароля
         if True in check_password.values():
             for k, v in check_password.items():
-                if str(v) == 'True':
+                if str(v) == 'True':  # вывод конкретной "неправильности" пароля
                     return render_template('register.html', title='Регистрация',
                                            form=form, message=k)
-        if form.hashed_password.data != form.password_again.data:
+        if form.hashed_password.data != form.password_again.data:  # проверка сходства паролей
             return render_template('register.html', title='Регистрация',
                                    form=form,
                                    message="Пароли не совпадают")
-        db_sess = db_session.create_session()
+        db_sess = db_session.create_session()  # проверка на существующий акк
         if db_sess.query(User).filter(User.email == form.email.data).first():
             return render_template('register.html', title='Регистрация',
                                    form=form,
@@ -64,10 +48,11 @@ def register():
         user = User(
             username=form.username.data,
             email=form.email.data,
-        )
+        )  # добавление пользователя в базу
         user.set_password(form.hashed_password.data)
         db_sess.add(user)
         db_sess.commit()
+        # переброс пользователя на страницу входа в профиль
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
 
@@ -78,7 +63,7 @@ def load_user(user_id):
     return db_sess.query(User).get(user_id)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])  # вход в профиль
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -89,37 +74,56 @@ def login():
             login_user(user, remember=form.remember_me.data)
             return redirect("/")
         return render_template('login.html',
-                               message="Неправильный логин или пароль",
+                               message="Неправильная почта или пароль",
                                form=form)
     return render_template('login.html', title='Авторизация', form=form)
 
 
-@app.route('/logout')
+@app.route('/logout')  # обработчик выхода из профиля
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
 
-@app.route("/")
-@app.route("/test")
+''' Нужно написать сreate_post.html, форму для создания статьи '''
+@app.route('/create_post', methods=['POST', 'GET'])  # обработчик создания статей
 @login_required
-def test():
-    return render_template("test.html", title='Test tongle')
+def create_post():
+    if request.method == 'POST':
+        creator = request.form['creator']
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+        post = Posts(creator=creator, title=title, intro=intro, text=text)
+        try:
+            db_session.add(post)
+            db_session.commit()
+            return redirect('/home')
+        except:
+            return 'Error!'
+    else:
+        return render_template('сreate_post.html')
 
 
-@app.errorhandler(404)
+''' Дописать post_detail.html '''
+@app.route('/posts/<int:id>')  # обработчик просомтра статьи
+def post_detail(id):
+    post = Posts.query.get(id)
+    return render_template('post_detail.html', post=post)
+
+
+@app.errorhandler(404)  # обработчик ошибок
 def page_not_found(e):
     return render_template('404.html'), 404
 
 
-def main():  # main function
+def main():  # главная функция приложния
     db_session.global_init("db/database.db")
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
     app.run(debug=True)
 
 
-# start server
-if __name__ == '__main__':
+if __name__ == '__main__':  # запуск приложения
     main()
