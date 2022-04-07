fb1b10bd6b24418dca99a9464a47336a810109c8
arrssenii <95125556+arrssenii@users.noreply.github.com>
2022-04-07 18:10:56 +0300

    start alfa 1.3

--- main.py
+++ main.py
@@ -3,19 +3,28 @@ from newsapi import NewsApiClient
 
 newsapi = NewsApiClient(api_key='5d4e4058e8c84af4badd8f726ac7dc6d')
 
-response = newsapi.get_everything(q='bitcoin',
-                                  language='ru',
-                                  sort_by='popularity',
-                                  page=1)
-
-print(response)
 
 app = Flask(__name__)
+
 
 @app.route('/')
 @app.route('/news')
 def news():
-    return f"<a href={response['articles'][0]['url']}>{response['articles'][0]['title']}</a>"
+    responses = []
+    for i in range(5):
+        j = i + 1
+        response = newsapi.get_everything(q='россия',
+                                          language='ru',
+                                          sort_by='popularity',
+                                          page=j)
+        print(response)
+        responses.append(response)
+    print(responses)
+    return f"<a href={responses[0]['articles'][0]['url']}>{responses[0]['articles'][0]['title']}</a></br>"\
+           f"<a href={responses[1]['articles'][0]['url']}>{responses[1]['articles'][0]['title']}</a></br>"\
+           f"<a href={responses[2]['articles'][0]['url']}>{responses[2]['articles'][0]['title']}</a></br>"\
+           f"<a href={responses[3]['articles'][0]['url']}>{responses[3]['articles'][0]['title']}</a></br>"\
+           f"<a href={responses[4]['articles'][0]['url']}>{responses[4]['articles'][0]['title']}</a></br>"
 
 
 if __name__ == '__main__':
