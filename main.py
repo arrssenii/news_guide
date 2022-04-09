6e4283161589adcd50433dd396de2274c07a470d
kirka00 <kirill.kobzev5555@gmail.com>
2022-04-10 00:18:57 +0300

    new

--- main.py
+++ main.py
@@ -140,12 +140,7 @@ def page_not_found(e):
     return render_template('404.html', title='404'), 404
 
 
-def main():  # главная функция приложния
+if __name__ == '__main__':  # запуск приложения
     db_session.global_init("db/database.db")
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
-    app.run(debug=True)
-
-
-if __name__ == '__main__':  # запуск приложения
-    main()
