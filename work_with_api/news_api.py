644c05a420cecb202370645bdbb3641a1714aaf2
kirka00 <kirill.kobzev5555@gmail.com>
2022-05-25 23:11:21 +0300

    tech

--- work_with_api/news_api.py
+++ work_with_api/news_api.py
@@ -16,3 +16,22 @@ else:  # если что-то пошло не так, выводим в конс
     print("Ошибка выполнения запроса:")
     print(request)
     print("Http статус:", response.status_code, "(", response.reason, ")")
+
+# работа с новостями про технологии
+request = "https://newsapi.org/v2/top-headlines?country=ru&category=technology&apiKey=499c7a59bd714f83abbee6644022628e"  # собираем запрос
+response = requests.get(request)  # Выполняем запрос.
+news_api_tech = []  # список для хранения будущих новостей
+if response:
+    json_response = response.json()  # Преобразуем ответ в json-объект
+    responce = json_response['articles']  # достаём новости
+    for article in responce:  # пробегаемся по ним, собирая всё "по полочкам"
+        source = article['source']['name']
+        title = article['title']
+        url = article['url']
+        publishedAt = article['publishedAt']
+        news_api_tech.append({'source': source, 'title': title, 'url': url,
+                         'publishedAt': publishedAt})  # добавляем всё, что нужно, в список
+else:  # если что-то пошло не так, выводим в консоль ошибку
+    print("Ошибка выполнения запроса:")
+    print(request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
