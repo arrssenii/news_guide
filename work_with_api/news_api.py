e576e0ca0455bddd2ce1ac116bb989e17a8c101f
kirka00 <kirill.kobzev5555@gmail.com>
2022-05-26 11:23:23 +0300

    firx and new

--- work_with_api/news_api.py
+++ work_with_api/news_api.py
@@ -1,37 +1,24 @@
 import requests
-request = "https://newsapi.org/v2/top-headlines?country=ru&apiKey=499c7a59bd714f83abbee6644022628e"  # собираем запрос
-response = requests.get(request)  # Выполняем запрос.
-news_api = []  # список для хранения будущих новостей
-if response:
-    json_response = response.json()  # Преобразуем ответ в json-объект
-    responce = json_response['articles']  # достаём новости
-    for article in responce:  # пробегаемся по ним, собирая всё "по полочкам"
-        source = article['source']['name']
-        title = article['title']
-        url = article['url']
-        publishedAt = article['publishedAt']
-        news_api.append({'source': source, 'title': title, 'url': url,
-                         'publishedAt': publishedAt})  # добавляем всё, что нужно, в список
-else:  # если что-то пошло не так, выводим в консоль ошибку
-    print("Ошибка выполнения запроса:")
-    print(request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
+from datetime import datetime
+def api_func(request):
+    response = requests.get(request)  # Выполняем запрос.
+    news_api = []  # список для хранения будущих новостей
+    if response:
+        json_response = response.json()  # Преобразуем ответ в json-объект
+        responce = json_response['articles']  # достаём новости
+        for article in responce:  # пробегаемся по ним, собирая всё "по полочкам"
+            source = article['source']['name']
+            title = article['title']
+            url = article['url']
+            publishedAt = article['publishedAt']
+            form_publishedAt = datetime.fromisoformat(publishedAt[:-1])
+            news_api.append({'source': source, 'title': title, 'url': url,
+                            'publishedAt': form_publishedAt.strftime('%d %B | %H:%M')})  # добавляем всё, что нужно, в список
+    else:  # если что-то пошло не так, выводим в консоль ошибку
+        print("Ошибка выполнения запроса:")
+        print(request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+    return news_api
 
-# работа с новостями про технологии
-request = "https://newsapi.org/v2/top-headlines?country=ru&category=technology&apiKey=499c7a59bd714f83abbee6644022628e"  # собираем запрос
-response = requests.get(request)  # Выполняем запрос.
-news_api_tech = []  # список для хранения будущих новостей
-if response:
-    json_response = response.json()  # Преобразуем ответ в json-объект
-    responce = json_response['articles']  # достаём новости
-    for article in responce:  # пробегаемся по ним, собирая всё "по полочкам"
-        source = article['source']['name']
-        title = article['title']
-        url = article['url']
-        publishedAt = article['publishedAt']
-        news_api_tech.append({'source': source, 'title': title, 'url': url,
-                         'publishedAt': publishedAt})  # добавляем всё, что нужно, в список
-else:  # если что-то пошло не так, выводим в консоль ошибку
-    print("Ошибка выполнения запроса:")
-    print(request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
+news_api = (api_func('https://newsapi.org/v2/top-headlines?country=ru&apiKey=499c7a59bd714f83abbee6644022628e'))
+news_api_tech = (api_func('https://newsapi.org/v2/top-headlines?country=ru&category=technology&apiKey=499c7a59bd714f83abbee6644022628e'))
